aa156b3611dc0834fe0384e9b4a6a2426279fadd
benjaminmiharris <benjmiharris@gmail.com>
2022-12-20 09:49:59 +0200

    routing bug fixes and error redirect if user not logged in

--- microblogging-app/src/components/Error404.js
+++ microblogging-app/src/components/Error404.js
@@ -1,23 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { UsernameContext } from "../context/UsernameContext";
+import { useContext } from "react";
 
 import Button from "react-bootstrap/Button";
 
 import "../style/error.css";
 
 const Error404 = () => {
+  const { isAuth } = useContext(UsernameContext);
+
   const navigate = useNavigate();
 
   const redirectHome = () => {
     navigate("/");
   };
 
+  const redirectLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <div>
       <div className="server-error">404</div>
       <div className="error-message">Oops! Looks like you are lost...</div>
-      <Button className="home-button" onClick={redirectHome}>
-        Return Home
-      </Button>
+
+      {isAuth ? (
+        <Button className="home-button" onClick={redirectHome}>
+          Return Home
+        </Button>
+      ) : (
+        <Button className="home-button" onClick={redirectLogin}>
+          Login
+        </Button>
+      )}
     </div>
   );
 };
